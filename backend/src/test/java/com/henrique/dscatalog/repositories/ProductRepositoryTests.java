b66f31d26ae72d262a22aa486c0dcdf11908f0ca
Henrique Pignatari Moraes Rosa <henrique.pignatarimoraes@gmail.com>
2024-06-18 17:23:24 -0300

    tests: adds `ProductRepository` save tests

--- backend/src/test/java/com/henrique/dscatalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/henrique/dscatalog/repositories/ProductRepositoryTests.java
@@ -1,7 +1,9 @@
 package com.henrique.dscatalog.repositories;
 
 import com.henrique.dscatalog.entities.Product;
+import com.henrique.dscatalog.tests.Factory;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -10,9 +12,30 @@ import java.util.Optional;
 
 @DataJpaTest
 public class ProductRepositoryTests {
+    private long existingId;
+    private long nonExistingId;
+    private long countTotalProducts;
+
     @Autowired
     private ProductRepository productRepository;
 
+    @BeforeEach
+    void setUp() throws Exception {
+        existingId = 1;
+        nonExistingId = 1000L;
+        countTotalProducts = 25;
+    }
+
+    @Test
+    public void saverShouldPersistWithAutoIncrementWhenIdIsNull(){
+        Product product = Factory.createProduct();
+        product.setId(null);
+
+        product = productRepository.save(product);
+
+        Assertions.assertNotNull(product.getId());
+        Assertions.assertEquals(countTotalProducts + 1, product.getId());
+    }
 
     @Test
     public void deleteShouldDeleteObjectWhenIdExists(){
