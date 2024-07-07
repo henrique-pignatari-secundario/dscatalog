4cce28b95512d2d6cbe24ffaf8f0f30f13f891fd
Henrique Pignatari Moraes Rosa <henrique.pignatarimoraes@gmail.com>
2024-07-06 23:55:53 -0300

    feat: adds security and config

---
+++ backend/src/main/java/com/henrique/dscatalog/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.henrique.dscatalog.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.authorizeHttpRequests(auth -> {
+            auth.anyRequest().permitAll();
+        });
+        http.securityMatcher(PathRequest.toH2Console()).csrf(AbstractHttpConfigurer::disable)
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
+        return http.build();
+    }
+}
